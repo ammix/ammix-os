ac333b997fb9e6500e8b5e2bbca1a26479161795
ammix <maxim@ammix.dev>
2025-04-28 23:48:50 +0200

    actual init

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,16 +9,68 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 copr enable -y atim/starship
+dnf5 copr enable -y kylegospo/webapp-manager
+dnf5 copr enable -y bieszczaders/kernel-cachyos-addons
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 install -y \
+  starship \
+  webapp-manager \
+  scx-scheds \
+  syncthing \
+  stow \
+  fzf \
+  ugrep \
+  fastfetch \
+  kitty \
+  steam-devices \
+  gamemode \
+  gum \
+  zathura \
+  zathura-fish-completion \
+  zathura-plugins-all \
+  wqy-zenhei-fonts \
+  google-roboto-fonts \
+  rsms-inter-fonts \
+  mozilla-fira-sans-fonts
 
-#### Example for enabling a System Unit File
+dnf5 remove -y firefox firefox-langpacks
 
+
+dnf5 -y copr disable atim/starship
+dnf5 -y copr disable kylegospo/webapp-manager
+dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
+
+## Fonts
+# Create directories
+mkdir -p /usr/share/fonts/nerd-fonts/FiraCode
+mkdir -p /usr/share/fonts/nerd-fonts/SymbolsOnly
+mkdir -p /usr/share/fonts/VictorMono
+
+curl -fLo /tmp/SymbolsOnly.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/NerdFontsSymbolsOnly.zip
+unzip /tmp/SymbolsOnly.zip -d /usr/share/fonts/nerd-fonts/SymbolsOnly/
+rm /tmp/SymbolsOnly.zip
+
+curl -fLo /tmp/FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip
+unzip /tmp/FiraCode.zip -d /usr/share/fonts/nerd-fonts/FiraCode/
+rm /tmp/FiraCode.zip
+
+curl -fLo /tmp/VictorMono_v1.5.6.zip https://github.com/rubjo/victor-mono/archive/refs/tags/v1.5.6.zip
+unzip -o /tmp/VictorMono_v1.5.6.zip -d /tmp/
+mv /tmp/victor-mono-1.5.6/otf/* /usr/share/fonts/VictorMono/
+rm /tmp/VictorMono_v1.5.6.zip
+rm -rf /tmp/victor-mono-1.5.6/
+
+fc-cache -f -v
+
+# Systemd
 systemctl enable podman.socket
+systemctl enable syncthing@maxim.service
+systemctl enable scx_loader.service
+
+# Plymouth
+plymouth-set-default-theme catppuccin-mocha
+dracut --force --regenerate-all
+
+# Cleanup
+rm -rf /tmp/*
