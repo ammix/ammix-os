c7be9ad4975b3112f1e0b8142f89e1105944a4e1
ammix <maxim@ammix.dev>
2025-04-30 00:09:46 +0200

    build file copy modification

--- Containerfile
+++ Containerfile
@@ -6,22 +6,13 @@ ARG FEDORA_VERSION=42
 
 FROM scratch AS ctx
 COPY build_files /
-
-FROM scratch AS stage-files
-COPY ./files /files
+COPY files /files/
 
 # Stage for akmods RPMs
 FROM ghcr.io/ublue-os/akmods:main-${FEDORA_VERSION} AS akmods
 
 # Base Image
 FROM ghcr.io/ublue-os/${BASE_IMAGE}:${FEDORA_VERSION}
-
-# Copy files
-RUN --mount=type=bind,from=stage-files,src=/files,dst=/tmp/files \
-    if [ -d /tmp/files/etc ]; then cp -a /tmp/files/etc/. /etc/; fi && \
-    if [ -d /tmp/files/usr ]; then cp -a /tmp/files/usr/. /usr/; fi && \
-    echo "File copy complete." && \
-    ostree container commit
 
 # Install akmods
 RUN --mount=type=cache,dst=/var/cache \
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,13 +2,10 @@
 
 set -ouex pipefail
 
-### Install packages
+# Copy files
+rsync -rv /ctx/files/. /
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
+# Install packages
 dnf5 copr enable -y atim/starship
 dnf5 copr enable -y kylegospo/webapp-manager
 dnf5 copr enable -y bieszczaders/kernel-cachyos-addons
@@ -36,6 +33,7 @@ dnf5 install -y \
   rsms-inter-fonts \
   mozilla-fira-sans-fonts
 
+# Uninstall Packages
 dnf5 remove -y \
   firefox \
   firefox-langpacks \
@@ -48,7 +46,7 @@ dnf5 -y copr disable che/nerd-fonts
 
 dnf5 clean packages
 
-## Fonts
+# Fonts
 fc-cache -f -v
 
 # Systemd
